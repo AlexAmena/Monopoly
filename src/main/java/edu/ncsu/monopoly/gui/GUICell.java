7073fccb55ac0ffd0e16bc18e69f2a5eb0b25209
EHU <a@a.com>
2022-10-05 10:27:12 +0200

    aa

--- src/main/java/edu/ncsu/monopoly/gui/GUICell.java
+++ src/main/java/edu/ncsu/monopoly/gui/GUICell.java
@@ -49,7 +49,7 @@ public class GUICell extends JPanel {
 			pnlPlayer.add(lblPlayers[i]);
 		}
 	}
-//aaaaaaa
+//aaaaaaaaaaa
 	public void displayInfo() {
 		lblInfo.setText(InfoFormatter.cellInfo(cell));
         this.invalidate();
